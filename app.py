218b91fbf7b06a05bd6fc76deb17dcc23badc73f
John <28509298+jsett@users.noreply.github.com>
2017-11-21 16:51:29 -0500

    feat: added redirect to index.html

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Dockerized'
+    return redirect("/static/index.html")
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
