2ff9371c52d91d13e3889ee93eb036ffb40d7215
John <28509298+jsett@users.noreply.github.com>
2017-11-21 18:56:27 -0500

    feat: added create to app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, request
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
     return redirect("/static/index.html")
+
+@app.route('/create', methods = ['POST'])
+def create():
+    print request.method
+    if request.method == 'POST':
+        print request.form
+    return "ok"
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
