8d6b99bc67236b1e7e242d6bde0629c78a77a8a9
John <28509298+jsett@users.noreply.github.com>
2017-11-28 19:18:38 -0500

    feat: added a rq test job.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 from flask import Flask, redirect, request
+from rq import Queue
+from redis import Redis
+from jobs import count_words_at_url
 app = Flask(__name__)
+
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
 
 @app.route('/')
 def index():
@@ -11,6 +17,19 @@ def create():
     #print "tag: " + request.form['btag']
     #return str(request.form["bookmark"])
     return redirect("/static/new.html")
+
+@app.route('/reddittest', methods = ['GET'])
+def reddittest():
+    #print "bookmark: " + request.form['bookmark']
+    #print "tag: " + request.form['btag']
+    #return str(request.form["bookmark"])
+    j = q.enqueue(count_words_at_url, 'http://reddit.com')
+    return str(j.id)
+
+@app.route('/redditres/<myid>', methods = ['GET'])
+def redditres(myid):
+    j = q.fetch_job(myid)
+    return str(j.result)
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
